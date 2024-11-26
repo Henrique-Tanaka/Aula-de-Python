046a67316bc8624a5fc628b770639e1f6c5e665d
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2024-11-25 22:34:33 -0300

    Update Exercício4.py

--- Exercícios/Exercício4.py
+++ Exercícios/Exercício4.py
@@ -4,7 +4,8 @@ Peça ao usuário para digitar seu nome
 Peça ao usuário para digitar sua idade
 Se nome e idade forem digitados:
     Exiba:
-        Seu nome é {nome}
+        Seu nome: {nome}
+        Sua idade: {idade}
         Seu nome invertido é {nome invertido}
         Seu nome contém (ou não) espaços
         Seu nome tem {n} letras
@@ -14,11 +15,10 @@ Se nada for digitado em nome ou idade:
     exiba "Desculpe, você deixou campos vazios."
 """
 
-nome = input('Digita seu nome: ')
-idade = input('Digita sua idade: ')
+nome = input('Seu nome: ')
+idade = input('Sua idade: ')
 if nome and idade:
-    print(f'Seu nome é {nome}')
-    print(nome[::-1])
+    print(f'Seu nome invertido é {nome[::-1]}')
     if ' ' in nome:
         print('Seu nome contem espaços')
     else:
@@ -26,5 +26,9 @@ if nome and idade:
     print(f'Seu nome tem {len(nome)} letras')
     print(f'A primeira letra do seu nome é {nome[0]}')
     print(f'A última letra do seu nome é {nome[-1]}')
+    if int(idade) >= 18:
+        print('Você é maior de idade.')
+    else:
+        print('Você é menor de idade')
 else:
     print('Desculpe, você deixou campos vazios.')
