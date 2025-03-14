dcab0fc44b9157a25f5392964556cfb46db9fe2e
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2025-03-14 16:54:18 -0300

    Create Exercício08.py

---
+++ Exercícios/Exercício08.py
@@ -0,0 +1,14 @@
+"""
+Faça isso:*n*o*m*e*
+"""
+nome = input('digite seu nome: ')
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
