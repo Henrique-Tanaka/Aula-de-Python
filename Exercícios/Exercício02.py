b87d64864d0698eb5645fc8a6676a742e117cee3
Henrique <168787470+Henrique-Tanaka@users.noreply.github.com>
2025-03-17 21:52:54 -0300

    Update Exercício02.py

--- Exercícios/Exercício02.py
+++ Exercícios/Exercício02.py
@@ -3,6 +3,6 @@ altura = 1.65
 peso = 53
 imc = peso / (altura ** 2)
 
-print('O nome dele é',nome+', ele mede',altura,'de altura.')
+print('O nome dele é',nome,', ele mede',altura,'de altura.')
 print(nome, 'pesa', peso, 'e o imc dele é: ')
 print(imc)
